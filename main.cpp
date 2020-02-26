be9a9f47cdae4898f3b30ab100a333d0ea6bba7b
David Lira <davidlira1909@gmail.com>
2020-02-26 23:28:38 +0100

    Input bala/cuadrado funcionales
    El cuadrado se mueve con las flechas y el proyectil se dispara con espacio
    (el cuadrado no para al dejar de pulsar una flecha)

--- main.cpp
+++ main.cpp
@@ -1,8 +1,5 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include <iostream>
 #include <math.h>
-#include <time.h>
 #include "SDL/include/SDL.h"
 #pragma comment( lib, "SDL/libx86/SDL2.lib" )
 #pragma comment( lib, "SDL/libx86/SDL2main.lib" )
@@ -10,10 +7,12 @@
 #define WINDOW_WIDTH	1920
 #define WINDOW_HEIGHT	1080
 
-int main(int argc, char* args[])
+int main(int argc, char* argv[])
 {
 	SDL_Window* Window;
 	SDL_Renderer* Renderer;
+
+	int cont = 0;
 
 	if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
 		SDL_Log("Unable to initialize SDL: %s", SDL_GetError());
@@ -34,46 +33,77 @@ int main(int argc, char* args[])
 		return -1;
 	}
 
-	int n = 0;
 	const int cx = WINDOW_WIDTH >> 1,
 		cy = WINDOW_HEIGHT >> 1;
-	SDL_Rect rc = { 960, 540, 80, 80 };
 
+
+	SDL_Rect rectangulo = { 920, 520, 80, 80 };
+	SDL_Rect bala = { 0, 0, 30, 30 };
 	SDL_Event event;
 	SDL_PollEvent(&event);
 
-	while (event.key.keysym.sym != SDLK_x)
+	while (event.key.keysym.sym != SDLK_ESCAPE)
 	{
-
-		SDL_SetRenderDrawColor(Renderer, 0, 0, 225, 0);
+		SDL_SetRenderDrawColor(Renderer, 0, 0, 255, 255);
 		SDL_RenderClear(Renderer);
 
-		if (event.key.keysym.sym == SDLK_w) {
-			rc.y += 5;
-		}
-		if (event.key.keysym.sym == SDLK_a) {
-			rc.x -= 5;
-		}
-		if (event.key.keysym.sym == SDLK_s) {
-			rc.y -= 5;
-		}
-		if (event.key.keysym.sym == SDLK_d) {
-			rc.x += 5;
-		}
-
-		SDL_SetRenderDrawColor(Renderer, 0, 0, 225, 0);
-		SDL_RenderClear(Renderer);
-		SDL_SetRenderDrawColor(Renderer, 225, 0, 0, 0);
-		SDL_RenderFillRect(Renderer, &rc);
-
-		SDL_Delay(500);
+		SDL_SetRenderDrawColor(Renderer, 255, 0, 0, 255);
+		SDL_RenderFillRect(Renderer, &rectangulo);
 
 		SDL_RenderPresent(Renderer);
+		SDL_PollEvent(&event);
 
-		n += 15;
+		if (event.key.keysym.sym == SDLK_DOWN) {
+			rectangulo.y += 1;
+			bala.x = rectangulo.x;
+			bala.y = rectangulo.y;
+		}
+		if (event.key.keysym.sym == SDLK_LEFT) {
+			rectangulo.x -= 1;
+			bala.x = rectangulo.x;
+			bala.y = rectangulo.y;
+		}
+		if (event.key.keysym.sym == SDLK_UP) {
+			rectangulo.y -= 1;
+			bala.x = rectangulo.x;
+			bala.y = rectangulo.y;
+		}
+		if (event.key.keysym.sym == SDLK_RIGHT) {
+			rectangulo.x += 1;
+			bala.x = rectangulo.x;
+			bala.y = rectangulo.y;
+		}
+
+
+		if (event.key.keysym.sym == SDLK_SPACE) {
+			if(cont==0){
+				bala.x = rectangulo.x;
+				bala.y = rectangulo.y;
+				cont++;
+			}
+			else {
+				bala.x += 2;
+				SDL_SetRenderDrawColor(Renderer, 0, 255, 0, 255);
+				SDL_RenderFillRect(Renderer, &bala);
+				SDL_RenderPresent(Renderer);
+			}
+		}
+
+		if (rectangulo.x >= 1920) {
+			rectangulo.x = 1;
+		}
+		if (rectangulo.x <= 0) {
+			rectangulo.x = 1920;
+		}
+		if (rectangulo.y >= 1080) {
+			rectangulo.y = 1;
+		}
+		if (rectangulo.y <= 0) {
+			rectangulo.y = 1080;
+		}
+
+		SDL_Delay(1);
 	}
-
 	SDL_Quit();
-
 	return 0;
 }
